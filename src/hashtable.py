0b48c1af7acbbb47d8e5040b320c68bb60385522
AndrewA0112 <andrew.mkal@outlook.com>
2020-01-14 17:13:54 -0600

    MVP Complete

--- src/hashtable.py
+++ src/hashtable.py
@@ -30,7 +30,6 @@ class HashTable:
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
@@ -47,47 +46,77 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        if self.count >= self.capacity:
-            self.resize()
-        
-        index = self._hash_mod(key)
-        if self.storage[index] is not None:
-            # Handle Collision
-            pass
-        self.storage[index] = value
-        self.count += 1
+
+        hash_key = self._hash_mod(key)
+
+        if self.storage[hash_key]:
+            entry = self.storage[hash_key]
+            while entry:
+                if entry.key == key: # Update the value
+                    entry.value = value
+                    break
+                elif entry.next: # Allows to loop back through the next entry, which is why this is in a while loop
+                    entry = entry.next
+                else: # Link the two items
+                    entry.next = LinkedPair(key, value)
+                    self.count += 1
+                    break
+        else:
+            self.storage[hash_key] = LinkedPair(key, value) # Instert LinkedPair
+            self.count += 1
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        index = self._hash_mod(key)
-        value = self.storage[index]
-        if value is None:
-            print('Error')
-        self.storage[index] = None
-        self.count -= 1
 
+        hash_key = self._hash_mod(key)
+
+        if self.storage[hash_key] is None:
+            print("No Key Found", key)
+            return
+
+        entry = self.storage[hash_key]
+        prev_entry = None
+        while entry: # Same concept of insert, allows to loop through pairs (if there is a pair)
+            # print(entry.key, key)
+            if entry.key == key: # Matches correct key
+                if prev_entry:
+                    if entry.next: # Link the two together since the connector of these two is about to be removed
+                        prev_entry.next = entry.next
+                    else:
+                        prev_entry.next = None
+                else:
+                    if entry.next: # Shifts over one to the left so the first index is now the second (only if there is a next)
+                        self.storage[hash_key] = entry.next
+                    else:
+                        self.storage[hash_key] = None # Where it gets reset
+                self.count -= 1
+                return entry.value
+            else:
+                prev_entry = entry
+                entry = entry.next
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
-        if self.storage[self._hash_mod(key)] is not None:
-            return self.storage[self._hash_mod(key)]
+
+        hash_key = self._hash_mod(key)
+
+        if self.storage[hash_key]:
+            entry = self.storage[hash_key]
+            while entry:
+                if entry.key == key:
+                    return entry.value
+                entry = entry.next
         return None
 
 
@@ -95,16 +124,21 @@ class HashTable:
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
-        self.capacity *= 2
-        new_storage = [None] * self.capacity
-        for i in range(self.count):
-            new_storage[i] = self.storage[i]
-        self.storage = new_storage
+        new_storage = []
+        for entry in self.storage:
+            while entry:
+                new_storage.append([entry.key, entry.value])
+                entry = entry.next
 
-
+        self.capacity = 2*self.capacity
+        self.storage = [None]*self.capacity
+        
+        for entry in new_storage :
+            self.insert(entry[0], entry[1])
+            self.count -= 1
+        return
 
 if __name__ == "__main__":
     ht = HashTable(2)
